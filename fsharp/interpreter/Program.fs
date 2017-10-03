dc3af59d1de6ab80076e4d40f65636d62f783f0f
Emaro <emaro@users.noreply.github.com>
2017-10-03 21:42:40 +0200

    Reorganize project
    Practise python and f#

---
+++ fsharp/interpreter/Program.fs
@@ -0,0 +1,15 @@
+﻿
+type Token(typ, value) =
+    member this.Type = typ
+    member this.Value = value
+    member this.ToString =  sprintf "Type: %s, Value: %O" this.Type this.Type
+
+    
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    
+    let t = Token("INTEGER", 5)
+    printf "%O" t
+    
+    0 // return an integer exit code
